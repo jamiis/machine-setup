b97db762c3392252afc965501346ccc70130a578
Jamis Johnson <jamismanwaring@gmail.com>
2014-05-29 15:15:21 -0400

    outline script for setting up a new machine

---
+++ gearup.py
@@ -0,0 +1,22 @@
+'''
+Yes, 'tis blank for now.
+This script will consolidate the setup of a new machine.
+I am placing it in this "how to setup a new machine" 
+repo because, well, this repo is meant to setup a new machine.
+Kewl. Don't have time right now so brb in the near future.
+'''
+
+# dotfiles
+
+# git
+#   don't have?
+#       install
+#   aliases, color, username, email
+
+# python / pip et al.
+
+# haskell / cabal
+
+# node / npm
+
+# ruby / rvm
