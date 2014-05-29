a4031ab7656277623dd676dd83983f99e9b25174
Jamis Johnson <jamismanwaring@gmail.com>
2014-05-29 15:19:22 -0400

    add git config notes

--- gearup.py
+++ gearup.py
@@ -7,6 +7,14 @@ Kewl. Don't have time right now so brb in the near future.
 '''
 
 # dotfiles
+# git config --global alias.co checkout
+# git config --global alias.br branch
+# git config --global alias.ci commit
+# git config --global alias.st status
+# NOTE: do name and email dynamically
+# git config --global user.name "Jamis Johnson"
+# git config --global user.email "jamis@seen.co"
+# git config --global color.ui true
 
 # git
 #   don't have?
